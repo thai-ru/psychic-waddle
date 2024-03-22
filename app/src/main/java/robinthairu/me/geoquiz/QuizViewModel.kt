dc27350127db1e50cee784e491929097e3467838
thai-ru <robinthairu34@gmail.com>
2024-03-22 07:45:26 +0300

    vdebugging

--- app/src/main/java/robinthairu/me/geoquiz/QuizViewModel.kt
+++ app/src/main/java/robinthairu/me/geoquiz/QuizViewModel.kt
@@ -1,10 +1,8 @@
 package robinthairu.me.geoquiz
 
-import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-private const val TAG = "QuizViewModel"
 const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
 class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
     private val questionBank = listOf(
@@ -17,7 +15,7 @@ class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel(
         Question(R.string.question_nairobi, false))
 
     private var currentIndex: Int
-        get() = savedStateHandle.get(CURRENT_INDEX_KEY) ?: 0
+        get() = savedStateHandle[CURRENT_INDEX_KEY] ?: 0
         set(value) = savedStateHandle.set(CURRENT_INDEX_KEY, value)
 
     val currentQuestionAnswer: Boolean
