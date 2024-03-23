0585c7762b3509ad7c25119faed3e86ddf64ba49
thai-ru <robinthairu34@gmail.com>
2024-03-23 23:03:04 +0300

    new activity binding

--- app/src/main/java/robinthairu/me/geoquiz/CheatActivity.kt
+++ app/src/main/java/robinthairu/me/geoquiz/CheatActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import robinthairu.me.geoquiz.databinding.ActivityCheatBinding
 
 class CheatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCheatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cheat)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
