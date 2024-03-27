9de53808e5136e11943c1879fb83e090399f95eb
thai-ru <robinthairu34@gmail.com>
2024-03-27 16:18:23 +0300

    model

--- app/src/main/java/robinthairu/me/geoquiz/QuizViewModel.kt
+++ app/src/main/java/robinthairu/me/geoquiz/QuizViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+const val IS_CHEATER_KEY = "IS_CHEATER_KEY"
 class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
     private val questionBank = listOf(
         Question(R.string.question_africa, false),
@@ -13,6 +14,10 @@ class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel(
         Question(R.string.question_mideast, false),
         Question(R.string.question_oceans, true),
         Question(R.string.question_nairobi, false))
+
+    var isCheater: Boolean
+        get() = savedStateHandle.get(IS_CHEATER_KEY) ?: false
+        set(value) = savedStateHandle.set(IS_CHEATER_KEY, value)
 
     private var currentIndex: Int
         get() = savedStateHandle[CURRENT_INDEX_KEY] ?: 0
